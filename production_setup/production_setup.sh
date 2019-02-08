6c2ff45285c66f7e9e3e4f45c6ea655ed5add873
brett <vande060@gmail.com>
2019-02-07 22:34:47 -0800

    add prod folder

---
+++ production_setup/production_setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Reference:
+# https://www.digitalocean.com/community/tutorials/how-to-serve-flask-applications-with-gunicorn-and-nginx-on-ubuntu-18-04
+
+apt-get update
+
+apt-get install python3-pip python3-dev nginx
+
+pip3 install virtualenv
+
+cd ~
+
+git clone https://github.com/brettvanderwerff/Portfolio-Website
+
+cd Portfolio-Website
+
+virtualenv env
+
+source env/bin/activate
+
+pip3 install gunicorn
+
+pip3 install -r requirements.txt
+
+cp /home/brett/Portfolio-Website/production_setup/Portfolio-Website.service \
+/etc/systemd/system/Portfolio-Website.service
+
+systemctl start Portfolio-Website
+
+systemctl enable Portfolio-Website
+
+cp /home/brett/Portfolio-Website/production_setup/Portfolio-Website \
+/etc/nginx/sites-available/Portfolio-Website
+
+ln -s /etc/nginx/sites-available/Portfolio-Website /etc/nginx/sites-enabled
+
+nginx -t
+
+systemctl restart nginx
+
+ufw delete allow 5000
+
+ufw allow 'Nginx Full'
